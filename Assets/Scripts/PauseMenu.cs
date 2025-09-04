12f4a06a6fa4e52d2e0f2a11160e361c031b279e
Kami <kami@kami.kami>
2025-09-04 12:19:55 +0300

    Menu Pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    
+    public static bool gameIsPaused = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+       
+    }
+      void Paused()
+        {
+            //désactiver le mouvement
+            PlayerMovement.instance.enabled = false;
+            //activer le menu
+            pauseMenuUI.SetActive(true);
+            //arrêter le temps
+            Time.timeScale = 0;
+            //cxharger le statut du jeu
+            gameIsPaused = true;
+
+        }
+      public void Resume()
+        {
+            //inverse de Paused
+            PlayerMovement.instance.enabled = false;
+
+            pauseMenuUI.SetActive(false);
+           
+            Time.timeScale = 1;
+            
+            gameIsPaused = false;
+
+        }
+     public void LoadMainMenu()
+        {
+            //Appeler pour éviter de charger les éléments
+            DontDestroyOnLoadScene.instance.RemoveFromDestroyOnLoad();
+            //Appeler la fonction Resume pour remettre le timeScale à 1
+            Resume();
+            //Chargement de la scène
+            SceneManager.LoadScene("MainMenu");
+        }
+}
